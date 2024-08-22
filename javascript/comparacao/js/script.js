59f2515f50da0f6dad98de67b78d35b45de4e4da
ErikaRVieir <ericaribeiro14.03@gmail.com>
2024-08-22 11:12:56 -0300

    javascript0

---
+++ javascript/comparacao/js/script.js
@@ -0,0 +1,38 @@
+//Comparação
+const numeroA =2
+const numeroB =8
+console.log( numeroA<numeroB);
+console.log(numeroA >numeroB);
+const numeroC=20;
+const numeroD=20;
+console.log(numeroC>=numeroD);
+console.log(numeroC<=numeroD);
+
+//comparação com log igualdade estrita
+const a = 2;
+const b = 2;
+console.log(a===b);//valor e tipo
+
+//comparação de igualdade ampla
+const c = 10
+const d="10"
+console.log(c===d);
+console.log(c==d); //valor e converter o tipo
+
+//direrenciação ESTRITA 
+const E= 3
+const F= 4
+console.log(E!==F)
+
+//Diferenciação Ampla
+const g=1
+const h="1"// onverte isso para inteiro
+console.log(g!=h);//1 é diferente de 1
+
+//empty value
+let x;
+let y=null;
+let z ="";
+console.log(x);
+console.log(y);
+console.log(z);
---
+++ javascript/objetodoconsole/js/script.js
@@ -0,0 +1,14 @@
+//Exibindo uma mensagem no console
+console.warn("Atenção:Aviso!");
+//Exibindo uma mensagem de error
+console.error("Ocorreu erro");
+//Exibindo uma mensagem informativa no console
+console.info("Informativa");
+// Criando uma grupo no console
+console.group("Mensagem de teste");
+console.log("Atente-se passo 1");
+console.log("Execute passo 2");
+console.log("Execute o final");
+console.groupEnd();
+//Limpar o console
+//console.clear();
